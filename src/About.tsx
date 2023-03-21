cec6d40fe7822b59395cda0d6aeae89ddd840139
john joshua desierto <joshdesierto@gmail.com>
2023-03-22 01:00:06 +0400

    added tooltip for techonology icons

--- src/About.tsx
+++ src/About.tsx
@@ -1,26 +1,23 @@
 import Technology from './Technology';
 import TechnologyIcon from './TechnologyIcon';
-import web from './assets/web.png'
-import frontend from './assets/frontend.png'
-import html from './assets/html.png'
-import css from './assets/css.png'
-import js from './assets/js.png'
-import ts from './assets/ts.png'
-import php from './assets/php.png'
-import sass from './assets/sass.png'
-
-import git from './assets/git.png'
-import vscode from './assets/vscode.png'
-
-import angular from './assets/angular.png'
-import bootstrap from './assets/bootstrap.png'
-
-import jquery from './assets/jquery.png'
-import laravel from './assets/laravel.png'
-import octobercms from './assets/octobercms.png'
-import react from './assets/react.png'
-import tailwindcss from './assets/tailwindcss.png'
-import wordpress from './assets/wordpress.png'
+import web from './assets/web.png';
+import frontend from './assets/frontend.png';
+import html from './assets/html.png';
+import css from './assets/css.png';
+import js from './assets/js.png';
+import ts from './assets/ts.png';
+import php from './assets/php.png';
+import sass from './assets/sass.png';
+import git from './assets/git.png';
+import vscode from './assets/vscode.png';
+import angular from './assets/angular.png';
+import bootstrap from './assets/bootstrap.png';
+import jquery from './assets/jquery.png';
+import laravel from './assets/laravel.png';
+import octobercms from './assets/octobercms.png';
+import react from './assets/react.png';
+import tailwindcss from './assets/tailwindcss.png';
+import wordpress from './assets/wordpress.png';
 
 function About() {
 
@@ -39,37 +36,39 @@ function About() {
             <div className='mb-[60px]'>
               <div className='text-white text-[16px] font-semibold mb-[20px]'>Languages i speak</div>
               <div className='flex gap-[20px] flex-wrap'>
-                <TechnologyIcon image={html}></TechnologyIcon>
-                <TechnologyIcon image={css}></TechnologyIcon>
-                <TechnologyIcon image={js}></TechnologyIcon>
-                <TechnologyIcon image={jquery}></TechnologyIcon>
-                <TechnologyIcon image={ts}></TechnologyIcon>
-                <TechnologyIcon image={sass}></TechnologyIcon>
-                <TechnologyIcon image={php}></TechnologyIcon>
+                <TechnologyIcon image={html} name="HTML"></TechnologyIcon>
+                <TechnologyIcon image={css} name="CSS"></TechnologyIcon>
+                <TechnologyIcon image={js} name="JavaScript"></TechnologyIcon>
+                <TechnologyIcon image={jquery} name="jQUery"></TechnologyIcon>
+                <TechnologyIcon image={ts} name="TypeScript"></TechnologyIcon>
+                <TechnologyIcon image={sass} name="Sass"></TechnologyIcon>
+                <TechnologyIcon image={php} name="PHP"></TechnologyIcon>
               </div>
             </div>
             <div className='mb-[60px]'>
               <div className='text-white text-[16px] font-semibold mb-[20px]'>Development tools i touch</div>
               <div className='flex gap-[20px] flex-wrap'>
-                <TechnologyIcon image={git}></TechnologyIcon>
-                <TechnologyIcon image={vscode}></TechnologyIcon>
+                <TechnologyIcon image={git} name="Git"></TechnologyIcon>
+                <TechnologyIcon image={vscode} name="VSCode"></TechnologyIcon>
+                <TechnologyIcon image={vscode} name="Gulp"></TechnologyIcon>
+                <TechnologyIcon image={vscode} name="Nunjucks"></TechnologyIcon>
               </div>
             </div>
             <div className='mb-[60px]'>
               <div className='text-white text-[16px] font-semibold mb-[20px]'>CMS technologies</div>
               <div className='flex gap-[20px] flex-wrap'>
-                <TechnologyIcon image={octobercms}></TechnologyIcon>
-                <TechnologyIcon image={wordpress}></TechnologyIcon>
+                <TechnologyIcon image={octobercms} name="OctoberCMS"></TechnologyIcon>
+                <TechnologyIcon image={wordpress} name="Wordpress"></TechnologyIcon>
               </div>
             </div>
             <div className='mb-[60px]'>
               <div className='text-white text-[16px] font-semibold mb-[20px]'>Frameworks</div>
               <div className='flex gap-[20px] flex-wrap'>
-                <TechnologyIcon image={tailwindcss}></TechnologyIcon>
-                <TechnologyIcon image={bootstrap}></TechnologyIcon>
-                <TechnologyIcon image={react}></TechnologyIcon>
-                <TechnologyIcon image={angular}></TechnologyIcon>
-                <TechnologyIcon image={laravel}></TechnologyIcon>
+                <TechnologyIcon image={tailwindcss} name="TailwindCSS"></TechnologyIcon>
+                <TechnologyIcon image={bootstrap} name="Bootstrap"></TechnologyIcon>
+                <TechnologyIcon image={react} name="React"></TechnologyIcon>
+                <TechnologyIcon image={angular} name="Angular"></TechnologyIcon>
+                <TechnologyIcon image={laravel} name="Laravel"></TechnologyIcon>
               </div>
             </div>
           </div>
--- src/TechnologyIcon.tsx
+++ src/TechnologyIcon.tsx
@@ -1,8 +1,11 @@
 import './TechnologyIcon.scss';
 
-function TechnlogyIcon({image}: {image: string}) {
-
-  return <div className='box2-wrap'><div className="box2 w-[75px] h-[75px] bg-contain bg-no-repeat bg-center" style={{backgroundImage: `url(${image})`}}></div></div>
+function TechnlogyIcon({image, name}: {image: string, name: string}) {
+  return  <div className='group box2-wrap cursor-pointer relative'>
+            <div className="box2 w-[75px] h-[75px] bg-contain bg-no-repeat bg-center" style={{backgroundImage: `url(${image})`}}>
+            </div>
+            <span className="absolute top-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">{name}</span>
+          </div>
 }
 
 export default TechnlogyIcon;
